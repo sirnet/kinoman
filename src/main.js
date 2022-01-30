66903e44f271ba11a33d93a52e6a9569b9550279
sirnet <artem@home-pc>
2022-01-30 15:53:20 +0300

    Вывел все DOM элементы

--- src/main.js
+++ src/main.js
@@ -1,9 +1,61 @@
 import { createSiteMenuTemplate } from './view/site-menu.js';
+import { creatSectionFilms, creatCardFilm } from './view/card-film.js';
+import { createStatsTemplate } from './view/stats.js';
+import { createShowMoreTemplate } from './view/show-more.js';
+import { createPopupTemlate } from './view/popup.js';
+import { createSectionTopFilms } from './view/top-rated.js';
+import { createMostCommentTemplated } from './view/most-commented.js';
 
-const siteMain = document.querySelector('.main');
+const FILM_COUNT = 4;
+const TOP_COUNT = 1;
+let filmsContainerTopList;
+const POSITION = 'beforeend';
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
+const searchElement = () => {
+  return document.querySelectorAll('.films-list__container');
 };
 
-render(siteMain, createSiteMenuTemplate(), 'beforeend');
+//Функция создания DOM элементов
+const render = (container, template) => {
+  container.insertAdjacentHTML(POSITION, template);
+};
+
+//Точка входа DOM элемента main
+const siteMain = document.querySelector('.main');
+render(siteMain, createSiteMenuTemplate());
+
+
+//Создание DOM элемента section films
+render(siteMain, creatSectionFilms());
+const filmsSection = document.querySelector('.films');
+
+//Создание DOM элемента карточек
+const filmsList = document.querySelector('.films-list__container')
+for (let i = 0; i <= FILM_COUNT; i++){
+  render(filmsList, creatCardFilm());
+}
+
+//Создание DOM элемента кнопка загрузки дополнительных фильмов
+render(filmsSection, createShowMoreTemplate());
+
+//Создание DOM элемента Top Film
+render(filmsSection, createSectionTopFilms());
+filmsContainerTopList = searchElement();
+for (let i = 0; i <=  TOP_COUNT; i++){
+  render(filmsContainerTopList[1], creatCardFilm());
+}
+
+render(filmsSection, createMostCommentTemplated());
+filmsContainerTopList = searchElement();
+for (let i = 0; i <=  TOP_COUNT; i++){
+  render(filmsContainerTopList[2], creatCardFilm());
+}
+
+//Создание DOM элемента статистики пользователя
+render(siteMain, createStatsTemplate());
+
+
+console.log(filmsContainerTopList);
+//render(siteMain, topFilmList, 'beforeend');
+//Создание DOM элемента popup
+//render(siteMain, createPopupTemlate, 'beforeend');
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,11 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
-  devtool: 'source-map',
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
-    watchContentBase: true,
-  }
+  devtool: 'source-map'
 };
